621eeb077b1635bfd221f9ce8caf719db385c90f
Panpan 3 <basile.lewan@gmail.com>
2019-12-04 13:56:14 +0100

    Chemins continus

--- projetJava/src/Modeles/CaseChemin.java
+++ projetJava/src/Modeles/CaseChemin.java
@@ -1,20 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modeles;
 
 import javafx.scene.image.ImageView;
 
-/**
- *
- * @author Basile
- */
+
+
 public class CaseChemin extends Case {
     private int x, y; // 0 = haut, 1 = droite etc
     
     private String ressource ;
+    
+    private int nbChemin;
+
     
     public CaseChemin(int x, int y, ImageView imgv){
         super(imgv,x , y);
@@ -24,5 +21,8 @@ public class CaseChemin extends Case {
     public CaseChemin() {
         super(null,-1 , -1);
         
+    }
+        public int getNbChemin() {
+        return nbChemin;
     }
 }
