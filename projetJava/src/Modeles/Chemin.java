cb01b9d17a15fd35d9b2279a43626ea3cfdcfa59
Panpan 3 <basile.lewan@gmail.com>
2019-12-04 13:54:25 +0100

    Chemins continus

--- projetJava/src/Modeles/Chemin.java
+++ projetJava/src/Modeles/Chemin.java
@@ -28,6 +28,8 @@ public class Chemin extends Observable {
    
    private boolean continu = true ;
    
+   private int startSymbol = 0;
+   
    
    public Chemin()
    {
@@ -38,7 +40,12 @@ public class Chemin extends Observable {
    }
     
      public void startDD(int c, int r) {
-        // TODO
+        if (!(grille[c][r] instanceof Symbole)){
+            continu = false;
+        }else{
+            startSymbol = Symbole(grille[c][r]).getNbSymbole();
+        }
+        
         
         this.stopped = false ;
         System.out.println("startDD : " + c + "-" + r);
@@ -77,20 +84,58 @@ public class Chemin extends Observable {
         notifyObservers();
     }
     
+        public void stopDD(int c, int r) {
+        
+        // mémoriser le dernier objet renvoyé par parcoursDD pour connaitre la case de relachement
+        
+        if (grille[c][r] instanceof Symbole){
+            if (startSymbol != grille[lastC][lastR].getNbSymbole()){
+                continu = false;            
+        }else{
+            continu = false;
+        }
+        grille.setContinu(startSymbol, continu);
+         
+        System.out.println("stopDD : " + c + "-" + r + " -> " + lastC + "-" + lastR);
+        this.stopped = true ;
+        
+        //grille[r][c] = 1 ;
+        
+                for ( int i = 0; i< 5 ; i++)
+        {
+            
+             for ( int j = 0; j< 5 ; j++)
+             {
+                 System.out.print(this.grille[i][j] +"  ");
+                 
+             }
+            
+             System.out.println("");
+        }
+        
+        
+        
+        setChanged();
+        notifyObservers();
+    }
+    
     public void parcoursDD(int c, int r) {
         // TODO
         lastC = c;
         lastR = r;
         this.started = false ;
         
-        grille[r][c] = new CaseChemin(r,c,null) ;
+        if (grille[r][c] instanceof CaseChemin){
+            grille.setContinu(grille[r][c].getNbChemin, false);
+        }
+        
+        grille[r][c] = new CaseChemin(r,c,null, startSymbol) ;
         lastChemin.add(grille[r][c]) ;
         
         if(lastChemin.size() > 3)
         {
-            
-            lastChemin.remove() ;
             System.out.println(lastChemin);
+            lastChemin.remove() ;
         }
         System.out.println("parcoursDD : c " + c + "- r " + r);
          setChanged();
